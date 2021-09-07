930a2f267a1ab9e0134e54c84f1cc6e2033db894
hari-testpress <89839073+hari-testpress@users.noreply.github.com>
2021-09-07 11:39:32 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://hari-mysite1.herokuapp.com','localhost',
+ALLOWED_HOSTS = ['https://mysite-hari.herokuapp.com/','localhost',
 '127.0.0.1']
 
 
